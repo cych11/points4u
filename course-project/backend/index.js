8f673e2460be21e5c58771dc5f7add75bae5ef33
chungcyr <cyrus.chung@mail.utoronto.ca>
2025-12-12 12:52:30 -0500

    made index.js go on port 3000 by default

--- course-project/backend/index.js
+++ course-project/backend/index.js
@@ -2,22 +2,7 @@
 require('dotenv').config();
 'use strict';
 
-const port = (() => {
-    const args = process.argv;
-
-    if (args.length !== 3) {
-        console.error("usage: node index.js port");
-        process.exit(1);
-    }
-
-    const num = parseInt(args[2], 10);
-    if (isNaN(num)) {
-        console.error("error: argument must be an integer.");
-        process.exit(1);
-    }
-
-    return num;
-})();
+const port = process.env.PORT || 3000;
 
 const express = require("express");
 const cors = require("cors");
